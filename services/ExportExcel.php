73e01f97f55a285f776d1fbe41cf9f88338ebbd5
ammar <bodla.pu@gmail.com>
2021-03-16 02:18:12 -0700

    excel file edited

--- services/ExportExcel.php
+++ services/ExportExcel.php
@@ -24,12 +24,12 @@ class ExportExcel extends connection
         $date2=$_REQUEST['ed'];
 
 
-            $sql = "select id, name, business_type, lot_no, street_name, post_code, state, xy, 
-			area_building_name_neighbourhood, city_name, image_path, grab_street, alternative_name
-			from poi_data where date_time::date>='$date1'::date and date_time::date<='$date2'::date and name is not null and business_type is not null  and
+            $sql = 'select id, name as "POI Name", business_type as "Business Type", lot_no as "Lot No", street_name as "Street Name", post_code as "Post Code", state as "State", xy, 
+			area_building_name_neighbourhood as "Area", city_name as "City Name", image_path as "Photo", grab_street as "Grab Street", alternative_name as "Alternative Name"
+			from poi_data where date_time::date>='."'".$date1."'".'::date and date_time::date<='."'".$date2."'".'::date and name is not null and business_type is not null  and
                 street_name is not null and  post_code is not null and state is not null and xy is not null 
                 and area_building_name_neighbourhood is not null and city_name is not null and image_path is not null and
-                grab_street is not null and image_path<>'';";
+                grab_street is not null and image_path<>'."''".';';
 
           //  echo $sql;
 		  
