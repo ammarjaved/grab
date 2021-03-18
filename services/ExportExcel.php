4ad1dd39f0756838caa3a2f5eb39690cf8f4578f
ammar <bodla.pu@gmail.com>
2021-03-17 23:39:23 -0700

    created by added to excel

--- services/ExportExcel.php
+++ services/ExportExcel.php
@@ -29,7 +29,7 @@ class ExportExcel extends connection
 
 
             $sql = 'select id, name as "POI Name", business_type as "Business Type", lot_no as "Lot No", street_name as "Street Name", post_code as "Post Code", state as "State", xy, 
-			area_building_name_neighbourhood as "Area", city_name as "City Name", image_path as "Photo", grab_street as "Grab Street", alternative_name as "Alternative Name"
+			area_building_name_neighbourhood as "Area", city_name as "City Name", image_path as "Photo", grab_street as "Grab Street", alternative_name as "Alternative Name",created_by
 			from poi_data where date_time::date>='."'".$date1."'".'::date and date_time::date<='."'".$date2."'".'::date and name is not null and business_type is not null  and
                 street_name is not null and  post_code is not null and state is not null and xy is not null 
                 and area_building_name_neighbourhood is not null and city_name is not null and image_path is not null and
@@ -47,6 +47,11 @@ class ExportExcel extends connection
 			   $pic=explode('/',$path);
 			   $size=sizeof($pic)-1;
 			   $row["Photo"]=$date1."_".$date2.'/'.$pic[$size];
+
+            $sql_status11="select user_name from tbl_users where id=". $row["created_by"];
+            $result_query11=pg_query($sql_status11);
+            $row1 = pg_fetch_assoc($result_query11);
+            $row["created_by"]=$row1['user_name'];
 			   
 			//   echo $pic[$size];
             if($status=='yes') {
